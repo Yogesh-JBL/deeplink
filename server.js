8d28ba62d029f42f33b07db8579bb78705cedfe9
Jainam-Dev <jainam-dev@Jainam-Devs-Mac-mini-2.local>
2025-12-03 11:47:32 +0530

    commit 5

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ app.get('/ref', (req, res) => {
   // Use secure:true only when using HTTPS (production). For local dev you can set secure:false.
   res.cookie('ref_session', cookieValue, {
     // DO NOT set httpOnly (Safari may not persist it before App Store redirect)
-    secure: IS_PROD,        // true on Vercel (HTTPS). false on local http.
+    httpOnly: false,
+    secure: true,        // true on Vercel (HTTPS). false on local http.
     sameSite: 'None',
     path: '/',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
